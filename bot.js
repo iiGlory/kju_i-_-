93771f0671f629a1b4960107e2efb66be1099897
iiGlory <45216946+iiGlory@users.noreply.github.com>
2018-12-07 11:35:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,53 @@ var channel = "515593865575661578";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Glory is the best , Glory is the best , Glory is the best , Glory is the beste , Glory is the best , Glory is the best , Glory is the best , **')
     },305);
+ 
+    client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(/)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+
+// -say
+
+ if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
+  }
+
+  
+
+ 
+
+
+if (command == "embed") {
+
+    let say = new Discord.RichEmbed()
+
+    .setDescription(args.join(" "))
+
+    .setColor(0x23b2d6)
+
+    message.channel.sendEmbed(say);
+
+    message.delete();
+
+  }
+
+
+
 });
 
 
